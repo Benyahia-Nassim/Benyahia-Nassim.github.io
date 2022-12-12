aa28daee9dc19c956c70db12e47ee99c574d54f6
Benyahia-Nassim <104031538+Benyahia-Nassim@users.noreply.github.com>
2022-12-12 17:59:21 +0100

    Add files via upload

---
+++ my-site/core/my-site.js
@@ -0,0 +1,32 @@
+var myName="Nassim";
+console.log(myName); 
+
+var  movieActors="Brad Pitt";
+console.log(movieActors);
+
+var carModel="Renault"; 
+console.log(carModel);
+
+/* string*/
+
+var  myAge="32";
+console.log(myAge);
+
+var yearBorn="1990";
+console.log(yearBorn);
+
+var dateVictory="2022"
+console.log(dateVictory);
+
+/*number*/
+
+let winner= true;
+console.log(true);
+
+let looser= false;
+console.log(false);
+
+let bigger= true;
+console.log(true);
+
+/*boolean*/
